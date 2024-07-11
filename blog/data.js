7ae7966092eb9e6104315e7a05a2529fcb877f35
Sahil <sahilsharma86600@gmail.com>
2024-07-11 19:41:24 +0530

    add blog

---
+++ blog/data.js
@@ -0,0 +1,74 @@
+const blogData = [
+  {
+    id: 1,
+    title: "Nice Workplace For Brainstorming Meetings",
+    date: "06 Nov 2016",
+    author: "David Miller",
+    image: "https://cdn.easyfrontend.com/pictures/blog/blog_3.jpg",
+    description:
+      "Unique side projects are the best place to innovate specially at your work.",
+    content: "<h1>fsed</h1>",
+  },
+  {
+    id: 2,
+    title: "My Adventure in Alps, The Highest!",
+    date: "29 Feb 2019",
+    author: "Shawn Paul",
+    image: "https://cdn.easyfrontend.com/pictures/blog/blog_6.jpg",
+    description:
+      "Being creative within the constraints of client briefs, budgets and timelines.",
+    content: "Complete blog post content for blog 2...",
+  },
+  {
+    id: 3,
+    title: "How I Escape My Mind By Listening To Music",
+    date: "26 Oct 2016",
+    author: "Elleca Perry",
+    image: "https://cdn.easyfrontend.com/pictures/blog/blog_8.jpg",
+    description:
+      "Assumenda non repellendus distinctio nihil dicta sapiente, quibusdam maiores.",
+    content: "Complete blog post content for blog 3...",
+  },
+  {
+    id: 4,
+    title: "How I Escape My Mind By Listening To Music",
+    date: "26 Oct 2016",
+    author: "Elleca Perry",
+    image: "https://cdn.easyfrontend.com/pictures/blog/blog_8.jpg",
+    description:
+      "Assumenda non repellendus distinctio nihil dicta sapiente, quibusdam maiores.",
+    content: "Complete blog post content for blog 3...",
+  },
+  {
+    id: 5,
+    title: "How I Escape My Mind By Listening To Music",
+    date: "26 Oct 2016",
+    author: "Elleca Perry",
+    image: "https://cdn.easyfrontend.com/pictures/blog/blog_8.jpg",
+    description:
+      "Assumenda non repellendus distinctio nihil dicta sapiente, quibusdam maiores.",
+    content: "Complete blog post content for blog 3...",
+  },
+  {
+    id: 6,
+    title: "How I Escape My Mind By Listening To Music",
+    date: "26 Oct 2016",
+    author: "Elleca Perry",
+    image: "https://cdn.easyfrontend.com/pictures/blog/blog_8.jpg",
+    description:
+      "Assumenda non repellendus distinctio nihil dicta sapiente, quibusdam maiores.",
+    content: "Complete blog post content for blog 3...",
+  },
+  {
+    id: 7,
+    title: "How I Escape My Mind By Listening To Music",
+    date: "26 Oct 2016",
+    author: "Elleca Perry",
+    image: "https://cdn.easyfrontend.com/pictures/blog/blog_8.jpg",
+    description:
+      "Assumenda non repellendus distinctio nihil dicta sapiente, quibusdam maiores.",
+    content: "Complete blog post content for blog 3...",
+  },
+];
+
+export default blogData;
